d6e6ffd3f31ae8965e5a692f698255bab34a0a66
Juan I. Vicente Prieto <jivp1991@gmail.com>
2022-07-31 19:14:22 -0300

    31.07.2022

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-dark text-light bg-dark">
                 <div className="container-fluid">
                     <Link to='/'>
-                    <img className="navbar-brand" src='navbar.png' width='70' />
+                    <img className="navbar-brand" src='./images/navbar.png' width='70' />
                     </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -24,6 +24,11 @@ const Navbar = () => {
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/Promociones">Promociones</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg></Link>
                             </li>
                         </ul>
                     </div>
