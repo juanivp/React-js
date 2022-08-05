156d01988d3e709695376e093de7db72b67db201
Juan I. Vicente Prieto <jivp1991@gmail.com>
2022-08-04 21:49:42 -0300

    item counter 04.08.2022

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Inicio from './components/pages/Inicio';
 import Productos from './components/pages/Productos';
 import Promociones from './components/pages/Promociones';
 import ItemListContainer from './components/navigation/ItemListContainer';
+import Contador from './components/navigation/Contador';
 
 function App() {
-
   return (
     <div className="App">
       <Router>
         <Navbar />
         <ItemListContainer />
+        <Contador stock={12} initial={1} />
         <Routes>
           <Route path='/' exact component={Inicio} />
           <Route path='/Productos' exact component={Productos} />
---
+++ src/components/navigation/Contador.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+
+const Contador = (props) => {
+    console.log(props.stock, props.initial); /* Valor máximo del contador */
+
+    const [clicks, setClicks] = React.useState(props.initial); /* hook de estado */
+
+    const handleIncrement = () => {
+        if (clicks < props.stock) {
+            setClicks(clicks + 1);
+        }
+    };
+
+    const handleDecrement = () => {
+        if (clicks > props.initial) {
+            setClicks(clicks - 1);
+        }
+    };
+
+    const onAdd = () => {
+        if (props.stock > 0) {
+            alert(`Agregaste ${clicks} unidades al carrito.`)
+        }
+        else {
+            alert(`No hay suficiente stock de este producto.`)
+        }
+    }
+
+    return (
+
+        <div>
+            <h3>Item Counter</h3>
+            <button onClick={handleDecrement}>restar</button>
+            <h3>Clicks: {clicks}</h3>
+            <button onClick={handleIncrement}>sumar</button>
+            <hr />
+            <button onClick={onAdd}>Finalizar Compra</button>
+        </div>
+    )
+}
+
+    export default Contador
+    //     const [clicks, setClicks] = useState(1);
+
+    //     function handleClicks() {
+    //         setClicks(clicks+1);
+    //     }
+
+
+    //   return (
+    //     <>
+    //         <button className='btn btn-light' onClick={handleClicks}>boton que quiero mostrar</button>
+    //         <h3>Clicks: {clicks}</h3>
+    //     </>
+    //   )
+
+
+
